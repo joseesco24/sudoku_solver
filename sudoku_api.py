ff8fd0ea5011996ab671ac70c44a71291189e34f
jose <josed.escobar@outlook.com>
2021-05-19 15:35:05 -0500

    cambios en la lectura de los mensajes de los archivos yaml

--- sudoku_api.py
+++ sudoku_api.py
@@ -1,6 +1,5 @@
-from logging import error
-from yaml_reader import load_http_response_messages_dict
-from yaml_reader import load_log_messages_dict
+from yaml_reader import load_http_response_message
+from yaml_reader import load_log_message
 from logs_printer import print_log
 
 from aiohttp.web_request import Request
@@ -11,14 +10,11 @@ import os
 api_key = "7bC47Aa517f3eC4BF7F29ee84dc0D5E3"
 script_firm = "api"
 
-http_messages = load_http_response_messages_dict()
-log_messages = load_log_messages_dict()
-
 api_routes = web.RouteTableDef()
 api = web.Application()
 
 
-def check_if_request_body_is_correct(request_body: dict) -> bool:
+def check_request_body(request_body: dict) -> bool:
     request_body_keys = [key for key in request_body.keys()]
     return True
 
@@ -36,46 +32,46 @@ async def solver(request: Request):
 
     if continue_process is True:
         if "Authorization" in request_header_keys:
-            print_log(log_messages["lm_001"], script_firm)
+            print_log(load_log_message("lm_001"), script_firm)
         else:
-            print_log(log_messages["lm_006"], script_firm)
+            print_log(load_log_message("lm_006"), script_firm)
             error_message_key = "hm_004"
             continue_process = False
 
     if continue_process is True:
         if api_key == request_headers["Authorization"]:
-            print_log(log_messages["lm_002"], script_firm)
+            print_log(load_log_message("lm_002"), script_firm)
         else:
-            print_log(log_messages["lm_005"], script_firm)
+            print_log(load_log_message("lm_005"), script_firm)
             error_message_key = "hm_003"
             continue_process = False
 
     if continue_process is True:
         if request.body_exists:
-            print_log(log_messages["lm_003"], script_firm)
+            print_log(load_log_message("lm_003"), script_firm)
         else:
-            print_log(log_messages["lm_004"], script_firm)
+            print_log(load_log_message("lm_004"), script_firm)
             error_message_key = "hm_002"
             continue_process = False
 
     if continue_process is True:
-        if check_if_request_body_is_correct(request_body):
-            print_log(log_messages["lm_008"], script_firm)
+        if check_request_body(request_body):
+            print_log(load_log_message("lm_008"), script_firm)
         else:
-            print_log(log_messages["lm_007"], script_firm)
+            print_log(load_log_message("lm_007"), script_firm)
             error_message_key = "hm_005"
             continue_process = False
 
     if continue_process is True and error_message_key is None:
         return web.Response(
-            reason=http_messages["hm_001"],
+            reason=load_http_response_message("hm_001"),
             body=json.dumps(obj=request_body, indent=None),
             status=200,
         )
 
     elif continue_process is False and error_message_key is not None:
         return web.Response(
-            reason=http_messages[error_message_key],
+            reason=load_http_response_message(error_message_key),
             status=400,
         )
 
--- yaml_reader.py
+++ yaml_reader.py
@@ -1,18 +1,15 @@
 import yaml
 
-http_response_messages_dict = dict()
-log_messages_dict = dict()
 
-with open(r"./static_messages_files/static_http_response_messages.yaml") as yaml_file:
-    http_response_messages_dict = yaml.load(stream=yaml_file, Loader=yaml.FullLoader)
-
-with open(r"./static_messages_files/static_log_messages.yaml") as yaml_file:
-    log_messages_dict = yaml.load(stream=yaml_file, Loader=yaml.FullLoader)
+def load_http_response_message(message_key: str) -> str:
+    yaml_path = r"./static_messages_files/static_http_response_messages.yaml"
+    with open(yaml_path) as yaml_file:
+        yaml_dict = yaml.load(stream=yaml_file, Loader=yaml.FullLoader)
+    return yaml_dict[message_key]
 
 
-def load_http_response_messages_dict() -> dict:
-    return http_response_messages_dict
-
-
-def load_log_messages_dict() -> dict:
-    return log_messages_dict
+def load_log_message(message_key: str) -> str:
+    yaml_path = r"./static_messages_files/static_log_messages.yaml"
+    with open(yaml_path) as yaml_file:
+        yaml_dict = yaml.load(stream=yaml_file, Loader=yaml.FullLoader)
+    return yaml_dict[message_key]
