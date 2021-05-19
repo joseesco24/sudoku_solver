91e2defbf447a41e6196f9b76a2a370b7f93fb38
jose <josed.escobar@outlook.com>
2021-05-19 17:35:01 -0500

    creacion de funcion de verificacion del request en el api

--- api.py
+++ api.py
@@ -14,52 +14,33 @@ api_routes = web.RouteTableDef()
 api = web.Application()
 
 
-def check_request_body(request_body: dict) -> bool:
-
-    requiered_keys = ["board", "area"]
-    the_request_body_is_correct = True
-
-    if the_request_body_is_correct is True:
-        if not type(request_body) is dict:
-            the_request_body_is_correct = False
-
-    if the_request_body_is_correct is True:
-        request_body_keys = [key for key in request_body.keys()]
-        if not all(key in request_body_keys for key in requiered_keys):
-            the_request_body_is_correct = False
-
-    if the_request_body_is_correct is True:
-        if not type(request_body["board"]) is list:
-            the_request_body_is_correct = False
-
-    if the_request_body_is_correct is True:
-        if not type(request_body["area"]) is list:
-            the_request_body_is_correct = False
-
-    if the_request_body_is_correct is True:
-        if not len(request_body["area"]) == 2:
-            the_request_body_is_correct = False
-
-    return the_request_body_is_correct
-
-
-@api_routes.get("/solver")
-async def solver(request: Request):
+async def check_request_requirements(request: Request):
 
     request_body = await request.json()
+    request_body_keys = [key for key in request_body.keys()]
 
     request_headers = request.headers
     request_header_keys = [key for key in request_headers.keys()]
 
-    error_message_key = None
+    necessary_fields_in_request = ["board", "area"]
+
+    message_key = "hm_001"
     continue_process = True
+
+    if continue_process is True:
+        if request.body_exists:
+            print_log(load_log_message("lm_003"), script_firm)
+        else:
+            print_log(load_log_message("lm_004"), script_firm)
+            message_key = "hm_002"
+            continue_process = False
 
     if continue_process is True:
         if "Authorization" in request_header_keys:
             print_log(load_log_message("lm_001"), script_firm)
         else:
             print_log(load_log_message("lm_006"), script_firm)
-            error_message_key = "hm_004"
+            message_key = "hm_004"
             continue_process = False
 
     if continue_process is True:
@@ -67,35 +48,61 @@ async def solver(request: Request):
             print_log(load_log_message("lm_002"), script_firm)
         else:
             print_log(load_log_message("lm_005"), script_firm)
-            error_message_key = "hm_003"
+            message_key = "hm_003"
             continue_process = False
 
     if continue_process is True:
-        if request.body_exists:
-            print_log(load_log_message("lm_003"), script_firm)
-        else:
-            print_log(load_log_message("lm_004"), script_firm)
-            error_message_key = "hm_002"
-            continue_process = False
-
-    if continue_process is True:
-        if check_request_body(request_body):
+        if type(request_body) is dict:
             print_log(load_log_message("lm_008"), script_firm)
         else:
-            print_log(load_log_message("lm_007"), script_firm)
-            error_message_key = "hm_005"
+            message_key = "hm_005"
             continue_process = False
 
-    if continue_process is True and error_message_key is None:
+    if continue_process is True:
+        if all(key in request_body_keys for key in necessary_fields_in_request):
+            pass
+        else:
+            continue_process = False
+
+    if continue_process is True:
+        if type(request_body["board"]) is list:
+            pass
+        else:
+            continue_process = False
+
+    if continue_process is True:
+
+        if type(request_body["area"]) is list:
+            pass
+        else:
+            continue_process = False
+
+    if continue_process is True:
+        if len(request_body["area"]) == 2:
+            pass
+        else:
+            continue_process = False
+
+    return continue_process, message_key
+
+
+@api_routes.get("/solver")
+async def solver(request: Request):
+
+    continue_process, message_key = await check_request_requirements(request)
+    reason_message = load_http_response_message(message_key)
+
+    if continue_process is True:
+        request_body = await request.json()
         return web.Response(
-            reason=load_http_response_message("hm_001"),
             body=json.dumps(obj=request_body, indent=None),
+            reason=reason_message,
             status=200,
         )
 
-    elif continue_process is False and error_message_key is not None:
+    else:
         return web.Response(
-            reason=load_http_response_message(error_message_key),
+            reason=reason_message,
             status=400,
         )
 
