47448b31e0e975de37a5cbedcd93549f4149bf19
jose <josed.escobar@outlook.com>
2021-05-19 21:04:41 -0500

    mejoras menores en el impresor de logs

--- logs_printer.py
+++ logs_printer.py
@@ -2,13 +2,10 @@ from datetime import datetime
 
 
 def print_log(message: str, script_firm: str = None) -> None:
-
     date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     message = str(message).capitalize()
-
     if script_firm is not None:
         script_firm = script_firm.upper()
         print(f"[{date_time}][{script_firm}] - {message}")
-
     else:
         print(f"[{date_time}] - {message}")
