2921e46439ca01de455b6a3f3cfe89104684da45
jose <josed.escobar@outlook.com>
2021-05-30 03:46:12 -0500

    se modifico la configuracion del balanceador de carga y del proxy, en el proxy ya funciona el desvio cuando el cuerpo de la peticion no se encuentra o no es un json

--- main_proxy_gateway_container/nginx_validator.js
+++ main_proxy_gateway_container/nginx_validator.js
@@ -1,2 +1,21 @@
-function check_request_mandatory_requirements(request) {}
-export default { check_request_mandatory_requirements };
+function check_request_mandatory_requirements(request) {
+    try {
+        if (request.variables.request_body) {
+            JSON.parse(request.variables.request_body);
+            request.error("request body checked successfully");
+            return request.variables.sucess_link;
+
+        } else {
+            request.error("request body not found");
+            return request.variables.error_link;
+        }
+
+    } catch (error) {
+        request.error("request body checked unsuccessfully");
+        return request.variables.error_link;
+    }
+}
+
+export default {
+    check_request_mandatory_requirements,
+};
