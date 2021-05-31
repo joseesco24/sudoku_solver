ab195bb073493ccbceede2ca40dbabecfc031fd9
jose <josed.escobar@outlook.com>
2021-05-31 00:46:39 -0500

    se eliminaron las validaciones del cuerpo de la peticion del contenedor de hill climbing y se mejoraron los docker file de los contenedores de hill climbing y de funciones generales para aprovechar mejor el cache por capas y construir las imagenes de una forma mas rapida si no se han cambiado especificamente los archivos de requerimientos

--- general_solvers_functions_container/production.Dockerfile
+++ general_solvers_functions_container/production.Dockerfile
@@ -4,8 +4,9 @@ ARG USERNAME=production
 ARG WORKDIR=/home/$USERNAME
 
 RUN useradd -ms /bin/bash $USERNAME
+RUN mkdir -p $WORKDIR/requirements/
 
-COPY [".","$WORKDIR"]
+COPY ["requirements/commons.txt","$WORKDIR/requirements/"]
 
 RUN find "$WORKDIR/" -type d -exec chmod 755 {} \;
 RUN find "$WORKDIR/" -type f -exec chmod 755 {} \;
@@ -20,3 +21,5 @@ USER $USERNAME
 
 RUN pip install --upgrade pip
 RUN pip install -r requirements/commons.txt
+
+COPY [".","$WORKDIR"]
--- hill_climbing_solver_container/api_server.py
+++ hill_climbing_solver_container/api_server.py
@@ -15,180 +15,63 @@ api = web.Application()
 script_firm = "api"
 
 
-async def check_request_mandatory_requirements(request: Request):
-
-    global script_firm
-
-    print_log(r"starting request body validations", script_firm)
-
-    try:
-        request_body = await request.json()
-        necessary_fields_in_request = ["board_array", "zone_length", "zone_height"]
-        request_header_keys = [key for key in request.headers.keys()]
-        request_body_keys = [key for key in request_body.keys()]
-        api_key = str(os.environ["HILL_CLIMBING_SOLVER_KEY"])
-        request_headers = request.headers
-        continue_process = True
-        print_log(r"the request body is correct", script_firm)
-    except:
-        continue_process = False
-        print_log(r"the request body is not correct", script_firm)
-
-    # Authorization header validations.
-
-    if continue_process is True:
-        if "Authorization" in request_header_keys:
-            print_log(r"the authorization header exists", script_firm)
-        else:
-            print_log(r"the authorization header dosn't exists", script_firm)
-            continue_process = False
-    if continue_process is True:
-        if api_key == request_headers["Authorization"]:
-            print_log(r"the authorization header is valid", script_firm)
-        else:
-            print_log(r"the authorization header isn't valid", script_firm)
-            continue_process = False
-
-    # Mandatory parameters existanse validations.
-
-    if continue_process is True:
-        if all(key in request_body_keys for key in necessary_fields_in_request):
-            print_log(r"the request body have all necesary labels", script_firm)
-        else:
-            print_log(r"the request body dosn't have all necessary labels", script_firm)
-            continue_process = False
-
-    # Mandatory parameters type validations.
-
-    if continue_process is True:
-        if type(request_body["board_array"]) is list:
-            print_log(
-                r"the variable board_array has the correct data type", script_firm
-            )
-        else:
-            print_log(
-                r"the variable board_array hasn't the correct data type", script_firm
-            )
-            continue_process = False
-    if continue_process is True:
-        if type(request_body["zone_length"]) is int:
-            print_log(
-                r"the variable zone_length has the correct data type", script_firm
-            )
-        else:
-            print_log(
-                r"the variable zone_length hasn't the correct data type", script_firm
-            )
-            continue_process = False
-    if continue_process is True:
-        if type(request_body["zone_height"]) is int:
-            print_log(
-                r"the variable zone_height has the correct data type", script_firm
-            )
-        else:
-            print_log(
-                r"the variable zone_height hasn't the correct data type", script_firm
-            )
-            continue_process = False
-
-    # Board dimensions validations.
-
-    if continue_process is True:
-        board_dimensions = request_body["zone_height"] * request_body["zone_length"]
-        board_height = len(request_body["board_array"])
-        if board_dimensions == board_height:
-            print_log(r"the board columns dimensions are correct", script_firm)
-        else:
-            print_log(r"the board columns dimensions aren't correct", script_firm)
-            continue_process = False
-    if continue_process is True:
-        board_dimensions = request_body["zone_height"] * request_body["zone_length"]
-        board_lenght_summation = 0
-        for board_row in request_body["board_array"]:
-            board_lenght_summation += len(board_row)
-        if board_dimensions == board_lenght_summation / board_dimensions:
-            print_log(r"the board rows dimensions are correct", script_firm)
-        else:
-            print_log(r"the board rows dimensions aren't correct", script_firm)
-            continue_process = False
-
-    if continue_process is True:
-        print_log(r"request body validation end successfully", script_firm)
-    else:
-        print_log(r"request body validation dosn't end successfully", script_firm)
-
-    return continue_process
-
-
 @api_routes.get(r"/solver")
 async def solver(request: Request):
 
-    continue_process = await check_request_mandatory_requirements(request)
+    restarts, searchs = 10, 10
 
-    if continue_process is True:
+    request_body = await request.json()
+    request_body_keys = [key for key in request_body.keys()]
 
-        restarts, searchs = 10, 10
+    sudoku_initial_board = request_body["board_array"]
+    sudoku_zone_height = request_body["zone_height"]
+    sudoku_zone_length = request_body["zone_length"]
 
-        request_body = await request.json()
-        request_body_keys = [key for key in request_body.keys()]
+    # Validation and search of specific solver parameters.
 
-        sudoku_initial_board = request_body["board_array"]
-        sudoku_zone_height = request_body["zone_height"]
-        sudoku_zone_length = request_body["zone_length"]
+    if "restarts" in request_body_keys:
+        if type(request_body["restarts"]) is int:
+            print_log(r"the variable restarts has the correct data type", script_firm)
+            restarts = request_body["restarts"]
+        else:
+            print_log(
+                r"the variable restarts hasn't the correct data type, using default value",
+                script_firm,
+            )
+    if "searchs" in request_body_keys:
+        if type(request_body["searchs"]) is int:
+            print_log(r"the variable searchs has the correct data type", script_firm)
+            searchs = request_body["searchs"]
+        else:
+            print_log(
+                r"the variable searchs hasn't the correct data type, using default value",
+                script_firm,
+            )
 
-        if "restarts" in request_body_keys:
-            if type(request_body["restarts"]) is int:
-                print_log(
-                    r"the variable restarts has the correct data type", script_firm
-                )
-                restarts = request_body["restarts"]
-            else:
-                print_log(
-                    r"the variable restarts hasn't the correct data type, using default value",
-                    script_firm,
-                )
-        if "searchs" in request_body_keys:
-            if type(request_body["searchs"]) is int:
-                print_log(
-                    r"the variable searchs has the correct data type", script_firm
-                )
-                searchs = request_body["searchs"]
-            else:
-                print_log(
-                    r"the variable searchs hasn't the correct data type, using default value",
-                    script_firm,
-                )
+    solution_board = await solve_using_hill_climbing_algorithm(
+        hill_climbing_restarts=restarts,
+        hill_climbing_searchs=searchs,
+        zone_height=sudoku_zone_height,
+        zone_length=sudoku_zone_length,
+        board=sudoku_initial_board,
+    )
 
-        solution_board = await solve_using_hill_climbing_algorithm(
-            hill_climbing_restarts=restarts,
-            hill_climbing_searchs=searchs,
-            zone_height=sudoku_zone_height,
-            zone_length=sudoku_zone_length,
-            board=sudoku_initial_board,
-        )
+    solution_board_fitness = await calculate_board_fitness_single(
+        zone_height=sudoku_zone_height,
+        zone_length=sudoku_zone_length,
+        board=solution_board,
+    )
 
-        solution_board_fitness = await calculate_board_fitness_single(
-            zone_height=sudoku_zone_height,
-            zone_length=sudoku_zone_length,
-            board=solution_board,
-        )
+    response_dict = {
+        "fitness_score": solution_board_fitness,
+        "board_array": solution_board,
+    }
 
-        response_dict = {
-            "fitness_score": solution_board_fitness,
-            "board_array": solution_board,
-        }
-
-        return web.Response(
-            body=json.dumps(obj=response_dict, indent=None),
-            reason=r"your request was successfully, check the results in the body of this response",
-            status=200,
-        )
-
-    else:
-        return web.Response(
-            reason=r"your request wasn't successfully, check the container logs for more details",
-            status=400,
-        )
+    return web.Response(
+        body=json.dumps(obj=response_dict, indent=None),
+        reason=r"your request was successfully, check the results in the body of this response",
+        status=200,
+    )
 
 
 api.add_routes(api_routes)
